f47198f68c76ab16e8d2b0198407d88343e8e33c
meemo <8729155+meemo@users.noreply.github.com>
2021-08-03 15:35:20 -0500

    Make README, merge main_rewrite.py to main.py

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # api.kwz.pw
- kwz.pw API Source Code.
+
+The kwz.pw API is intended to be used for accessing metadata and file contents from the Flipnote Studio 3D DSi Library file set.
+
+# Features
+
+These are the current features for the API, documentation will be created when the scope of the project expands.
+
+`http://api.kwz.pw/user/{fsid}/flipnotes?key={api key}`
+
+Returns a JSON list of all flipnotes with the given FSID as current author ID.
+
+`http://api.kwz.pw/flipnote/{file name}/meta?key={api key}`
+
+# Usage (testing)
+
+`pip3 install -r requirements.txt`
+
+### Bash
+
+ ```shell
+$ export FLASK_APP=main
+$ flask run
+```
+
+### Powershell
+
+```
+> $env:FLASK_APP = "main"
+> flask run
+```
+
+
+# Usage (production)
+
+`pip3 install -r requirements.txt`
+
+The rest to be written later.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import flask
+import json
 import psycopg2 as pg
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
 from src.fsid import convertFSID, validateFSID
 
 api_key = open("api_key.txt", "r").read().strip()
@@ -9,86 +10,24 @@ api_key = open("api_key.txt", "r").read().strip()
 # db_conn = pg.connect("host=localhost port=5432 dbname=flipnotes user=meta_import password = " + db_password)
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class user(Resource):
-    def get(self):
-        arg_parser = reqparse.RequestParser()
-
-        arg_parser.add_argument("key", required=True)
-        arg_parser.add_argument("fsid", required=True)
-        arg_parser.add_argument("scope", required=True)
-
-        args = arg_parser.parse_args()
-
-        if args["key"] == api_key:
-            if validateFSID(args["fsid"]):
-                if args["scope"] == "flipnotes":
-                    # All arguments are valid, proceed with request.
-
-                    print("worked")
-                else:
-                    return {"message": "Scope is invalid."}, 401
-            else:
-                return {"message": "FSID is invalid."}, 401
+# Return all flipnotes made by specified FSID as a json list
+@app.route("/user/<fsid>/flipnotes")
+def hello(fsid):
+    if flask.request.args.get("key") == api_key:
+        if validateFSID(fsid):
+            return {"message": "Request valid."}
         else:
-            return {"message": "API key is invalid or incorrect."}, 401
-
-    def post(self):
-        return {"message": "Request is not supported."}, 400
-
-    def head(self):
-        return {"message": "Request is not supported."}, 400
-
-    def put(self):
-        return {"message": "Request is not supported."}, 400
-
-    def delete(self):
-        return {"message": "Request is not supported."}, 400
-
-    pass
+            return {"message": "The specified FSID is invalid."}, 403
+    else:
+        return {"message": "The specified API key is invalid or incorrect."}, 401
 
 
-class flipnote(Resource):
-    def get(self):
-        arg_parser = reqparse.RequestParser()
-
-        arg_parser.add_argument("key", required=True)
-        arg_parser.add_argument("fsid", required=True)
-        arg_parser.add_argument("scope", required=True)
-
-        args = arg_parser.parse_args()
-
-        if args["key"] == api_key:
-            if args["scope"] == "meta":
-                # Assuming the file name is correct for now
-                # TODO: regex for file name
-                # All other arguments are valid, proceed with request.
-
-                print("worked")
-            else:
-                return {"message": "Scope is invalid."}, 401
-        else:
-            return {"message": "API key is invalid or incorrect."}, 401
-
-    def post(self):
-        return {"message": "Request is not supported."}, 400
-
-    def head(self):
-        return {"message": "Request is not supported."}, 400
-
-    def put(self):
-        return {"message": "Request is not supported."}, 400
-
-    def delete(self):
-        return {"message": "Request is not supported."}, 400
-
-    pass
-
-
-api.add_resource(user, "/user")
-api.add_resource(flipnote, "/flipnote")
-
-if __name__ == "__main__":
-    app.run()
+# Return all meta in the database for the given flipnote as a json object
+@app.route("/flipnote/<filename>/meta")
+def hello(filename):
+    if flask.request.args.get("key") == api_key:
+        return {"message": "Request valid."}
+    else:
+        return {"message": "The specified API key is invalid or incorrect."}, 401
